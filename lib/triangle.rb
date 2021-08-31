eba55b535f2b3c3a93f0fb42742487c04524e248
Coy Reid <coyreid7@gmail.com>
2021-08-31 16:59:00 -0500

    commit tree afa1a35ea4491bb9d6be4a2a6e2f81733ba2b24e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    triangle_validation
+    if a == b && b == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_ok
+    [a, b, c].all?(&:positive?)
+  end
+
+  def triangle_inequality?
+    a + b > c && b + c > a && a + c > b
+  end
+
+  def triangle_validation
+    raise TriangleError unless sides_ok && triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
